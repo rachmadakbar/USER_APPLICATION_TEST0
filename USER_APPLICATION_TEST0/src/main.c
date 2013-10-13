ec5ae20eb04bda605f3693c1bbd21623861dd0eb
nandax <m.nanda.k@aim.com>
2013-10-13 21:22:44 +0700

    Failed shooting
    Gagal shooting.

--- USER_APPLICATION_TEST0/src/main.c
+++ USER_APPLICATION_TEST0/src/main.c
@@ -76,6 +76,7 @@ static void change_text()
 }
 */
 uint16_t sun_value = 0;
+
 static void sun_count()
 {
 	sun_value++;
@@ -87,24 +88,28 @@ static void sun_count()
 }
 
 uint8_t zombie_position = 122; //in x
+
 static void zombie_walk()
 {
 	//if(gpio_pin_is_low(GPIO_PUSH_BUTTON_0)){
-	gfx_mono_draw_filled_rect(zombie_position+5, 8, 1, 8, GFX_PIXEL_CLR);
-	zombie_position--;
-	gfx_mono_draw_string("@", zombie_position, 8, &sysfont);
+	//gfx_mono_draw_filled_rect(zombie_position+5, 8, 1, 8, GFX_PIXEL_CLR);
+	//zombie_position--;
+	//gfx_mono_draw_string("@", zombie_position, 8, &sysfont);
 	//}
 }
 
+uint8_t cursor_position = 8; //in y
+
 uint8_t bullet_position = 13; //in x
+
 static void bullet_seed()
 {
-	gfx_mono_draw_filled_rect(bullet_position, 24, 1, 8, GFX_PIXEL_CLR);
+	gfx_mono_draw_filled_rect(bullet_position, cursor_position, 1, 8, GFX_PIXEL_CLR);
 	bullet_position++;
-	gfx_mono_draw_string("*", bullet_position, 24, &sysfont);
+	gfx_mono_draw_string("*", bullet_position, cursor_position, &sysfont);
 }
 
-uint8_t cursor_position = 8; //in y
+
 static void cursor_select()
 {
 	bool up_pressed = gpio_pin_is_low(GPIO_PUSH_BUTTON_1);
@@ -145,9 +150,29 @@ static void cursor_select()
 	if(planting && sun_value >= 10 && !check_plant[plant_index]){
 		gfx_mono_draw_string("P", 6, cursor_position, &sysfont);
 		sun_value -= 10;
-		check_plant[plant_index] = true;
+		uint8_t temp_p = plant_index; 
+		check_plant[temp_p] = true;
+		
+		//if(check_plant[temp_p]){gfx_mono_draw_string("POK", 70, 24, &sysfont);}
 	}
+	
+	char* buffer_check;
+	for(int c = 0; c < 3; c++){
+		sprintf(buffer_check, "%d", check_plant[c]);
+		gfx_mono_draw_string(buffer_check, 70, (c+1)*8, &sysfont);
+	}
+	//bool shooting = gpio_pin_is_high(QTOUCH_BUTTON_SNSK);
 
+	if(planting && check_plant[cursor_position]){
+		gfx_mono_draw_string("OK", 115, 24, &sysfont);
+
+		tc_enable(&TCE0);
+		tc_set_overflow_interrupt_callback(&TCE0, bullet_seed);
+		tc_set_wgm(&TCE0, TC_WG_NORMAL);
+		tc_write_period(&TCE0, 15625);
+		tc_set_overflow_interrupt_level(&TCE0, TC_INT_LVL_LO);
+		tc_write_clock_source(&TCE0, TC_CLKSEL_DIV64_gc);
+	}
 }
 
 int main (void)
@@ -198,13 +223,8 @@ int main (void)
 	tc_set_overflow_interrupt_level(&TCC1, TC_INT_LVL_LO);
 	tc_write_clock_source(&TCC1, TC_CLKSEL_DIV8_gc);
 	
-	tc_enable(&TCE0);
-	tc_set_overflow_interrupt_callback(&TCE0, bullet_seed);
-	tc_set_wgm(&TCE0, TC_WG_NORMAL);
-	tc_write_period(&TCE0, 15625);
-	tc_set_overflow_interrupt_level(&TCE0, TC_INT_LVL_LO);
-	tc_write_clock_source(&TCE0, TC_CLKSEL_DIV64_gc);
-	
+	/* bullet seed timer was moved to cursor_select() */
+		
 	tc_enable(&TCE1);
 	tc_set_overflow_interrupt_callback(&TCE1, zombie_walk);
 	tc_set_wgm(&TCE1, TC_WG_NORMAL);
